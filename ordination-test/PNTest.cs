db9f5444a22a73f8840502bd5cdfbf26ce2064e3
OskarLV <oskarstudiekonto@gmail.com>
2025-05-05 21:51:23 +0700

    PN test

--- ordination-test/PNTest.cs
+++ ordination-test/PNTest.cs
@@ -20,4 +20,35 @@ public class PNTest
         Assert.AreEqual(1, pn.dates.Count);
 
     }
-}
+    
+    
+    
+        [TestMethod]
+        public void OpretPNTest()
+        {
+            
+            
+            //her laver vi noget seeddata kan man sige til ordinationen 
+            var patient = new Patient("121256-7890", "Anna Hansen", 65.0);
+            var laegemiddel = new Laegemiddel("Paracetamol", 1, 1.5, 2.0, "ml");
+            var startDato = new DateTime(2025, 5, 1);
+            var slutDato = new DateTime(2025, 5, 10);
+            double antalEnheder = 2;
+            //her laver vi et PN objekt med overstånede data inden i 
+            var pnOrdination = new PN(startDato, slutDato, antalEnheder, laegemiddel);
+            
+            //Her bliver ordinationen puttet ind i den liste som af ordinationer som hver patient har 
+            patient.ordinationer.Add(pnOrdination);
+
+            
+            //Her tjekkes der om outputtet er som forventet 
+            Assert.AreEqual("PN", pnOrdination.getType());
+            Assert.AreEqual(startDato, pnOrdination.startDen);
+            Assert.AreEqual(slutDato, pnOrdination.slutDen);
+            Assert.AreEqual(antalEnheder, pnOrdination.antalEnheder);
+            Assert.AreEqual(1, patient.ordinationer.Count);
+            Assert.AreSame(pnOrdination, patient.ordinationer[0]);
+            
+        }
+    }
+
