8cfd5921c24d6aa3d0074f97ba862c22c91d89e0
Pakizasaal <psadat31@gmail.com>
2025-05-06 00:45:16 +0200

    PNTests

--- ordination-test/PNTest.cs
+++ ordination-test/PNTest.cs
@@ -10,7 +10,7 @@ public class PNTest
     public void givDosis_ValidDato()
     {
         var lm = new Laegemiddel("TestMedicin", 1, 1.0, 1.0, "ml");
-        var pn = new PN(new DateTime(2015, 5, 1), new DateTime(2015, 5, 5), 2, lm);
+        var pn = new PN(new DateTime(2025, 5, 1), new DateTime(2025, 5, 5), 2, lm);
         var dato = new Dato();
 
         dato.dato = new DateTime(2025, 5, 5);
@@ -19,5 +19,21 @@ public class PNTest
         Assert.IsTrue(result);
         Assert.AreEqual(1, pn.dates.Count);
 
+    }
+
+    [TestMethod]
+    public void givDosis_InvalidDato()
+    {
+        var lm = new Laegemiddel("TestMedicin", 1, 1.0, 1.0, "ml");
+        var pn = new PN(new DateTime(2025,5,1), new DateTime(2025,5,10), 2, lm);
+        var dato = new Dato();
+        
+        dato.dato = new DateTime(2025, 6, 1);
+        
+        var result = pn.givDosis(dato);
+        
+        Assert.IsFalse(result,"Should be false, date’s no good");
+        Assert.AreEqual(0, pn.getAntalGangeGivet(), "No doses should be saved");
+        
     }
 }
