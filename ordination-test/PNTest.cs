cd4b406d14ea862a3b5efb3376a0ea1b9148c5b6
Frederik <eaa23fogc@students.eaaa.dk>
2025-05-06 12:25:09 +0200

    tilføjet invalid igen.

--- ordination-test/PNTest.cs
+++ ordination-test/PNTest.cs
@@ -6,6 +6,7 @@ using shared.Model;
 public class PNTest
 {
 
+    
     [TestMethod]
     public void givDosis_ValidDato()
     {
@@ -22,6 +23,21 @@ public class PNTest
     }
     
     
+    [TestMethod]
+    public void givDosis_InvalidDato()
+    {
+        var lm = new Laegemiddel("TestMedicin", 1, 1.0, 1.0, "ml");
+        var pn = new PN(new DateTime(2025,5,1), new DateTime(2025,5,10), 2, lm);
+        var dato = new Dato();
+        
+        dato.dato = new DateTime(2025, 6, 1);
+        
+        var result = pn.givDosis(dato);
+        
+        Assert.IsFalse(result,"Should be false, date’s no good");
+        Assert.AreEqual(0, pn.getAntalGangeGivet(), "No doses should be saved");
+        
+    }
     
         [TestMethod]
         public void OpretPNTest()
